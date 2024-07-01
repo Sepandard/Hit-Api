beef7046d32cd88d32b848c2bf3e2dd22e301579
s.derakhshande <s.derakhshande@mydigipay.com>
2024-07-01 19:52:41 +0330

    feat(swagger) add endpoint to swagger

--- _swagger.js
+++ _swagger.js
@@ -4,6 +4,10 @@ const outputFile = './swagger_output.json';
 const endpointsFiles = [
     './modules/hit/controller/getHit.js',
     './modules/hit/controller/insertHit.js',
+    './modules/auth/controller/login/login.js',
+    './modules/auth/controller/signup/signup.js',
+    './modules/feedback/controller/getFeedback.js',
+    './modules/feedback/controller/insertFeedback.js',
 ];
 
 swaggerAutogen(outputFile, endpointsFiles);
